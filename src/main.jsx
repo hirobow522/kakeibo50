33be9d3731bbc4d1fe7c8d52d4e4f78576918d46
hirobow522 <86504956+hirobow522@users.noreply.github.com>
2025-06-15 06:55:42 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/Home.jsx';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Reactの表示テスト</h1>
-'    <Home />
+    <App />
   </React.StrictMode>
 );
